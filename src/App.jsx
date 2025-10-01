189ba11eecd3361e5780771c9903fd472a71ba24
Mizna PC <mizanur.wp2024@gmail.com>
2025-10-01 21:30:30 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+import DaisyNav from './assets/components/DaisyNav/DaisyNav'
+import Link from './assets/components/NavBar/Link'
+import NavBar from './assets/components/NavBar/NavBar'
+
+function App() {
+
+
+  return (
+    <>
+      <DaisyNav></DaisyNav>
+      <NavBar></NavBar>
+    
+
+    
+
+      <h1 className='text-2xl font-bold text-amber-600'>Md. Mizanur Rahman</h1>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/components/NavBar/NavBar.jsx
@@ -0,0 +1,50 @@
+
+import React from 'react';
+import Link from './Link';
+
+const navLinks = [
+  {
+    id: 1,
+    name: "Home",
+    path: "/"
+  },
+  {
+    id: 2,
+    name: "About",
+    path: "/about"
+  },
+  {
+    id: 3,
+    name: "Services",
+    path: "/services"
+  },
+  {
+    id: 4,
+    name: "Blog",
+    path: "/blog"
+  },
+  {
+    id: 5,
+    name: "Contact",
+    path: "/contact"
+  }
+];
+
+
+const NavBar = () => {
+  return (
+    <nav>
+      <ul className='flex'>
+                {
+          navLinks.map(route => <Link key={route.id} route={route}></Link>)
+        }
+
+      </ul>
+
+   
+    </nav>
+
+  );
+};
+
+export default NavBar;
