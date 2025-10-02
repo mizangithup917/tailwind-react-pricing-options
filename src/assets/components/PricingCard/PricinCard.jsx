1c10efd6653fdcadf8a48c06b9310720a7dc6e9b
Mizna PC <mizanur.wp2024@gmail.com>
2025-10-02 11:24:46 +0600

    card and daisy card make comments

--- src/assets/components/PricingCard/PricinCard.jsx
+++ src/assets/components/PricingCard/PricinCard.jsx
@@ -5,23 +5,27 @@ import PricingFeature from './PricingFeature';
 const PricinCard = ({pricing}) => {
   const {name, price, description, features} = pricing;
   return (
-    <div  className='border bg-amber-600 rounded-2xl p-4'>
+    
+    <div className='container mx-auto px-10'>
+        <div  className='flex flex-col border bg-amber-600 rounded-2xl p-4'>
       {/* card header */}
-      <div>
-        <h1 className='text-4xl'>{name}</h1>
-        <h1 className='text-2xl'>{price}</h1>
-      </div>
+        <div>
+          <h1 className='text-4xl text-white font-bold text-center'>{name}</h1>
+          <h1 className='text-2xl bg-[#F5F1DC] text-center mt-5 rounded-xl py-2'>{price}</h1>
+        </div>
       {/* card body */}
-      <div className='bg-amber-200 p-4 rounded-2xl mt-10'>
-        <p>{description}</p>
-        {
-          features.map((feature, index) => <PricingFeature 
-          key={index}
-          feature={feature}></PricingFeature>)
-        }
-      </div>
-
+        <div className='bg-amber-200 p-4 rounded-2xl mt-10 flex-1'>
+          <p>{description}</p>
+          {
+            features.map((feature, index) => <PricingFeature 
+            key={index}
+            feature={feature}></PricingFeature>)
+          }
+        </div>
+        <button className="bg-[#FF9D3D] py-2 rounded-2xl w-full hover:bg-[#F5F1DC] border-3 border-amber-100 font-bold mt-8 mb-5">Order Now</button>
+        </div>
     </div>
+ 
   );
 };
 
--- src/assets/components/PricingOptions/PricingOptions.jsx
+++ src/assets/components/PricingOptions/PricingOptions.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricinCard from '../PricingCard/PricinCard';
+import DaisyCard from '../DaisyCard/DaisyCard';
 
 const PricingOptions = ({pricingPromise}) => {
 
@@ -8,12 +9,18 @@ const pricingData = use(pricingPromise);
 
   return (
     <div>
-        <h2>Get our Membership</h2>
-        <div className='grid md:grid-cols-3 gap-8'>
+        <h2 className='text-5xl mb-10'>Get our Membership</h2>
+        <div className='grid md:grid-cols-3 gap-8 mx-auto'>
           {
             pricingData.map(pricing => <PricinCard 
               key={pricing.id}
               pricing={pricing}></PricinCard>)
+          }
+
+          {
+            pricingData.map(pricing=><DaisyCard
+            key={pricing.id}
+            pricing={pricing}></DaisyCard>)
           }
         </div>
     </div>
